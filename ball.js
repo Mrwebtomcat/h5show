f7777fe73abd28a93f2678788acaec7f73b93c24
Webconnect <13611496568@163.com>
2018-12-19 13:32:40 +0800

    完善参数

--- ball.js
+++ ball.js
@@ -1,15 +1,26 @@
 class Ball
 {
 	constructor(props){
-		this.x = props.x
-		this.y = props.y
+		this.x =0;
+		this.y = 0;
+		this.r = 20;
+		this.vx=0;
+		this.vy=0;
+		this.vz=0;
+		this.x3d = 0;
+		this.y3d = 0;
+		this.scaleX =1;
+		this.scaleY =1;
+		this.strokeStyle = 'rgb(0,0,0)';
+		this.fillStyle = 'rgb(57, 119, 224)';
+		this.alpha = 1;
 		Object.assigin(this,props);
 		return this;
 	}
 
 	
 	render(ctx){
-
+		
 	}
 
 }
